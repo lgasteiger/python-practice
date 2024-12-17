862710e56319e4882895a0c8aedbd18ef4a45102
Leo Genesis Gasteiger <Lgasteiger@gmail.com>
2024-12-16 21:58:22 -0800

    refactored numeric value validation

--- ch5/voting.py
+++ ch5/voting.py
@@ -1,22 +1,22 @@
 import time
 
-def test_age(person_age):
+def test_age():
     """
     this function tests if person_age is over 18 and eligible to vote
     """
     age_to_vote = 18
     try:
-        if isNumericInput(person_age):
-            if person_age >= age_to_vote:
-                print(
-                    f"great news, you are old enough to vote"
-                ) # end print()
-                isRegisteredVote()
-            else:
-                print(
-                    f"sorry, you are not eligile to vote yet. the legal age " +
-                    f"to vote in the U.S. is 18"
-                ) # print()
+        persons_age = int(input(f"please enter the age: "))
+        if persons_age >= age_to_vote:
+            print(
+                f"great news, you are old enough to vote"
+            ) # end print()
+            isRegisteredVote()
+        else:
+            print(
+                f"sorry, you are not eligile to vote yet. the legal age " +
+                f"to vote in the U.S. is 18"
+            ) # print()
             # end if
         # end if
     except TypeError as e:
@@ -29,25 +29,10 @@ def test_age(person_age):
         ) # end print()
     except Exception as e:
         print(
-            f"an unexpected, unhandle exception occured, {e}"
+            f"!!!!!an unexpected, unhandle exception occured, {e}!!!!!"
         ) # end print()
     # end if
 # end test_age()
-        
-def isNumericInput(ind_age):
-    """
-    this function validates if the individual'a age 'ind_age' is a numeric
-    value, and it will raise a TypeError exception 
-    """
-    if isinstance(ind_age, int):
-        return True
-    else:
-        raise TypeError(
-            f"!!!!!the value '{ind_age}' is not numeric, please try " +
-            f"again!!!!!"
-        ) # end TypeError() exceptionb
-    # end if
-# end isNumericInput(ind_age)
         
 def isRegisteredVote():
     """
@@ -61,7 +46,7 @@ def isRegisteredVote():
     if user_input != "y" and user_input != "Y" and \
         user_input != "n" and user_input != "N":
         raise ValueError(
-            "!!!!!please only respond with a 'y' or 'n' char!!!!!"
+            "please only respond with a 'y' or 'n' char"
         ) #end ValueError() exception
     # end if
 
@@ -80,9 +65,8 @@ def isRegisteredVote():
 # main app driver starts here #
 ###############################
 print("**********test age of person**********")
-person_age = 93
 start_time = time.time()
-test_age(person_age)
+test_age()
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(
