a4b5ad4f49e90b7dddba548aa10c4632d9120447
Leo Genesis Gasteiger <Lgasteiger@gmail.com>
2024-12-21 11:59:35 -0800

    refactored toppings_v2 and started alien_colors

---
+++ ch5/alien_colors.py
@@ -0,0 +1,10 @@
+import time
+
+def alien_status(last_event):
+    alien_color = "green"
+    
+# end alien_status()
+
+##########################
+# driver app starts hers #
+##########################
--- ch5/toppings_v2.py
+++ ch5/toppings_v2.py
@@ -2,26 +2,20 @@ import time
 
 def is_requested_anchovies(topping):
     """this function determines if anchovies was requested as a topping"""
-    try:
-        if topping != 'anchovies':
-            print(
-                f"since the requested topping, {requested_topping}, was " + 
-                f"not ordered, please hold the anchovies\n"
-            ) # end print()
-        # end if
-    except Exception as e:
+    if topping != 'anchovies':
         print(
-            f"!!!!!an unexpected, unhandled error occurred, {e}!!!!!\n"
+            f"since the requested topping, {topping}, wasn't anchovies, " + 
+            f"please hold the anchovies"
         ) # end print()
-    # end try...except
-# end is_requested_anchovies()
+    # end if
+# end is_requested_anchovies(topping)
         
 def is_continue():
     """
     this function prompts for a response to continuing the app
     """
     user_resp = input(
-        f"would you like to continue? (y/n)"
+        f"would you like to continue (y/n)? "
     ) # end user_resp
     
     # remove leading and trailing whitespace
@@ -41,11 +35,39 @@ def is_continue():
     # end if
 # end is_continue()
 
-def is_valid_topping(topping_req):
+def is_valid_input(topping_input, toppings_list):
     """
     this function verifies if the topping entered is valid 
     """
-# end is_valid_topping()
+    if not topping_input.strip():
+        raise ValueError(
+            f"the input is blank. please try again"
+        ) # end ValueError() exception
+    # end if
+
+    if topping_input in toppings_list:
+        raise ValueError(
+            f"the '{topping_input}' topping was already requested. " +
+            f"please try again"
+        ) # end ValueError() exception
+    # end if
+
+    return True
+# end is_valid_topping(topping_input, toppings_list)
+
+def print_pizza_toppings(toppings_ordered):
+    """
+    prints all the requested toppings for the pizza to the display
+    """
+    print(
+        f"*****the complete list of pizza toppings*****"
+    ) # end print()
+    for index, topping in enumerate(toppings_ordered):
+        print(
+            f"{index + 1}. {topping}"
+        ) # end print()
+    # end for...in
+# end print_pizza_toppings(toppings_ordered)
 
 def get_toppings():
     """
@@ -53,23 +75,57 @@ def get_toppings():
     """
     requested_toppings = []
     while True:
-        topping_entered = input(
-            f"please enter your preferred pizza topping: "
-        ) # end topping_entered
+        try:
+            topping_entered = input(
+                f"please enter your preferred pizza topping: "
+            ) # end topping_entered
 
-        # validate topping input
+            if is_valid_input(topping_entered, requested_toppings):
+                requested_toppings.append(topping_entered)
+                print(
+                    f"adding '{topping_entered} to the pizza"
+                ) # end print()
+                is_requested_anchovies(topping_entered)
+            # end if
 
-        # add topping to requested_toppings list
-        
+            if not is_continue():
+                break
+            # end if
+        except ValueError as e:
+            print(
+                f"!!!!!ValueError() exception was raised, {e}!!!!!"
+            ) # end print()
+
+            if not is_continue():
+                break
+            # end if
+        except IndexError as e:
+            print(
+                f"!!!!!IndexError() exception was raised, {e}"
+            ) # end print()
+
+            if not is_continue():
+                break
+            # end if
+        except Exception as e:
+            print(
+                f"!!!!!an unexpected, unhandled exception was raised, {e}!!!!!"
+            ) # end print()
+
+            if not is_continue():
+                break
+            # end if
+        # end try...except
     # end while
+                
+    print_pizza_toppings(requested_toppings)
 # end get_toppings()
 
 ###################
 # app starts here #
 ###################
 start_time = time.time()
-requested_topping = 'mushrooms'
-is_requested_anchovies(requested_topping)
+get_toppings()
 end_time = time.time()
 elapsed_time = end_time - start_time        
 print(
