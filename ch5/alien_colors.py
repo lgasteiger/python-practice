0bee2d6277301a9d4fed98d0e9345759271f903c
Leo Genesis Gasteiger <Lgasteiger@gmail.com>
2024-12-22 16:57:53 -0800

    updates to test drivers and conditions

--- ch5/alien_colors.py
+++ ch5/alien_colors.py
@@ -1,10 +1,76 @@
 import time
 
-def alien_status(last_event):
-    alien_color = "green"
-    
+def alien_status(color_event):
+    """
+    this function will track the status of the alien's events
+    """
+    if color_event.lower() == "green":
+        print(
+            f"you have just earned 5 points"
+        ) # end print()
+    elif color_event.lower() == "yellow":
+        print(
+            f"you have just earned 10 points"
+        ) # end print()
+    elif color_event.lower() == "red":
+        print(
+            f"you have just earned 15 points"
+        ) # end print()
+    # end if
 # end alien_status()
+        
+def test_alien_color_statuses(color_status):
+    """
+    this function will be the driver to test the various alien color statuses
+    """
+    try:
+        alien_status(color_status)
+    except ValueError as e:
+        print(
+            f"!!!!!ValueError() exception raised, {e}!!!!!"
+        ) # end print()
+    except Exception as e:
+        print(
+            f"!!!!!an unexpected, unhandled error occured, {e}!!!!!"
+        ) # end print()
+    # end try...except
+# end test_alien_color_statuses()
 
 ##########################
 # driver app starts hers #
 ##########################
+print(
+    f"*****testing 'green' alien status color*****"
+) # end print
+start_time = time.time()
+test_alien_color_statuses("green")
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(
+    f"the total time it took in seconds to finish the app functions = " +
+    f"{elapsed_time}\n"
+) # end print()
+
+print(
+    f"*****testing 'yellow' alien status color*****"
+) # end print
+start_time = time.time()
+test_alien_color_statuses("yellow")
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(
+    f"the total time it took in seconds to finish the app functions = " +
+    f"{elapsed_time}\n"
+) # end print()
+
+print(
+    f"*****testing 'red' alien status color*****"
+) # end print
+start_time = time.time()
+test_alien_color_statuses("red")
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(
+    f"the total time it took in seconds to finish the app functions = " +
+    f"{elapsed_time}\n"
+) # end print()
---
+++ ch5/life_stages.py
@@ -0,0 +1,155 @@
+import time
+
+def is_continue():
+    """
+    this function will prompt the user to continue the app's loop process
+    """
+    user_response = input(
+        f"would you like to continue? (y/n) "
+    ) # end user_response
+
+    if user_response != "y" and user_response != "Y" and \
+        user_response != "n" and user_response != "N":
+        raise ValueError(
+            f"only 'y' and 'n' input values are accepted. please try again"
+        ) # end ValueError() exception
+    # end if
+
+    if user_response == "n" or user_response == "N":
+        return False
+    else:
+        return True
+    # end if
+# end is_continue()
+    
+def identify_life_stage(person_age):
+    """
+    this function determines the stage of life based on a person's age 
+    """
+    if person_age < 2:
+        print(
+            f"the person is identified as a baby\n"
+        ) # end print()
+    elif person_age >= 2 and person_age < 4:
+        print(
+            f"the person is identified as a toddler\n"
+        ) # end print()
+    elif person_age >= 4 and person_age < 13:
+        print(
+            f"the person is identified as a kid\n"
+        ) # end print()
+    elif person_age >= 13 and person_age < 20:
+        print(
+            f"the person is identified as a teenager\n"
+        ) # end print()
+    elif person_age >= 20 and person_age < 65:
+        print(
+            f"the person is identified as an adult\n"
+        ) # print()
+    else:
+        print(
+            f"the person is identified as an elder\n"
+        ) # print()
+    # end if
+# end identify_life_stage(person_age)
+        
+def get_person_age():
+    """
+    this function prompts for the person's age and validates the data value
+    """
+    user_input = input(
+        f"please enter the person's age: "
+    ) # end user_input
+
+    if not user_input.strip():
+        raise ValueError(
+            f"the value inputted was blank. please try again"
+        ) # end ValueError() exception
+    # end if
+
+    if user_input.isdigit():
+        return int(user_input)
+    else:
+        raise ValueError(
+            f"the value inputted, '{user_input}', is not an age number. " +
+            f"please try again"
+        ) # end ValueError() exception
+# end get_person_age()
+    
+def test_life_stages():
+    """
+    this is the driver to test the app's stages of life conditionals
+    """
+    while True:
+        try:
+            inputted_val = get_person_age()
+            identify_life_stage(inputted_val)
+            
+            if not is_continue():
+                break
+            # end if
+        except ValueError as e:
+            print(
+                f"!!!!!ValueError() exception raised, {e}!!!!!"
+            ) # end print()
+
+            try:
+                if not is_continue():
+                    break
+                # end if
+            except ValueError as e:
+                print(
+                    f"!!!!!ValueError() exception raised, {e}!!!!!"
+                ) # end print()
+            except Exception as e:
+                print(
+                    f"!!!!!an unexpected, unhandled exception was raised, " +
+                    f"{e}!!!!!"
+                ) # end print()
+            # end try...except
+        except TypeError as e:
+            print(
+                f"!!!!!TypeError() exception raised, {e}!!!!!"
+            ) # end print()
+
+            try:
+                if not is_continue():
+                    break
+                # end if
+            except ValueError as e:
+                print(
+                    f"!!!!!ValueError() exception raised, {e}!!!!!"
+                ) # end print()
+            except Exception as e:
+                print(
+                    f"!!!!!an unexpected, unhandled exception was raised, " +
+                    f"{e}!!!!!"
+                ) # end print()
+            # end try...except
+        except Exception as e:
+            print(
+                f"!!!!!an unexpected, unhandled exception was raised, {e}!!!!!"
+            ) # end print()
+            
+            try:
+                if not is_continue():
+                    break
+                # end if
+            except ValueError as e:
+                print(
+                    f"!!!!!ValueError() exception raised, {e}!!!!!"
+                ) # end print()
+            except Exception as e:
+                print(
+                    f"!!!!!an unexpected, unhandled exception was raised, " +
+                    f"{e}!!!!!"
+                ) # end print()
+            # end try...except
+        # end try...except
+    # end while
+# end test_life_stages()
+
+############################
+# main program starts here #
+############################
+test_life_stages()
