aab721a6629898238a452525953e2b35375047d0
lgasteiger <lgasteiger@gmail.com>
2025-08-17 00:08:01 -0700

    adding alien dic data structure functions

--- ch6/alien.py
+++ ch6/alien.py
@@ -5,23 +5,32 @@ def get_dic_value(dic_name, dic_key):
     return dic_name[dic_key]
 # end get_points()
 
+def print_dic_contents(dic_name):
+    """
+    prints the key/value pairs of the dictionary, "dic_name"
+    """
+    for key, value in dic_name.items():
+        print(f"key -> {key}: value -> {value}")
+    # end for
+# end print_dic_contents()
+
 def add_dic_value(dic_name, dic_key, dic_value):
     """
-    This function add a new key: value pair to the dictionary.
+    this function add a new key: value pair to the dictionary
     """
     dic_name[dic_key] = dic_value
 # end add_dic_value()
     
 def update_dic_value(dic_name, dic_key, dic_value):
     """
-    This function will update the specified key value.
+    this function will update the specified key value
     """
     dic_name[dic_key] = dic_value
 # end update_dic_value()
     
 def determine_pos_inc(dic_name):
     """
-    This function will determine the increment value that will move the alien.
+    this function will determine the increment value that will move the alien
     """
     if dic_name['speed'] == 'slow':
         return 1
@@ -34,19 +43,29 @@ def determine_pos_inc(dic_name):
 
 def move_alien_position(dic_name):
     """
-    Move the alien to the right.
-    Determine how far to move the alien based on its current speed.
+    move the alien to the right
+    determine how far to move the alien based on its current speed
     """
     x_increment = determine_pos_inc(dic_name)
     dic_name['x_position'] = dic_name['x_position'] + x_increment
 # end move_alien_position()
+    
+def del_alien_value(dic_name, dic_key):
+    """
+    deletes the key/value pair of the dictionary
+    """
+    del dic_name[dic_key]
+# end del_alien_value()
 
 ########################
 # main app starts here #
 ########################
-alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {
+    'color': 'green',
+    'points': 5,
+} # end alien_0 dic
 
-print("*****Test getting dictionary values*****")
+print("*****test getting dictionary values*****")
 alien_color = get_dic_value(alien_0, "color")
 alien_points = get_dic_value(alien_0, "points")
 print(f"the alien's color is: {alien_color}")
@@ -56,23 +75,24 @@ new_points = get_dic_value(alien_0, "points")
 print(f"You just earned {new_points} points!")
 print()
 
-print("*****Test add new dictionary values*****")
+print("*****test add new dictionary values*****")
 add_dic_value(alien_0, "x_position", 0)
 add_dic_value(alien_0, "y_position", 25)
-print(alien_0)
+print_dic_contents(alien_0)
 print()
 
-print("*****Test updating a dictionary value*****")
+print("*****test updating a dictionary value*****")
 update_dic_value(alien_0, "color", "yellow")
-print(alien_0)
+print_dic_contents(alien_0)
 print()
 
-print("*****Test moving alien position based on present speed*****")
+print("*****test moving alien position based on present speed*****")
 add_dic_value(alien_0, "speed", "medium")
 update_dic_value(alien_0, "speed", "fast")
 move_alien_position(alien_0)
 print(f"New alien x-axis: {alien_0['x_position']}")
+print()
 
-# update_dic_value(alien_0, "speed", "fast")
-# move_alien_position(alien_0)
-# print(f"New alien x-axis: {alien_0['x_position']}")
+print("*****testing deleting alien dic key/value pair*****")
+del_alien_value(alien_0, "points")
+print_dic_contents(alien_0)
