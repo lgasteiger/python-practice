eef94ec7648bbddcdc17c430039d83130925f340
lgasteiger <lgasteiger@gmail.com>
2025-08-17 19:22:04 -0700

    refactored favorite languages functions use built-in get/setter dictionary methods

--- ch6/favorite_languages.py
+++ ch6/favorite_languages.py
@@ -24,4 +24,12 @@ favorite_languages = {
 
 print("*****test the get function to return a dictionary value*****")
 language = get_language(favorite_languages, 'sarah')
-print(f"Sarah's favorite langauge is: '{language}'.")
+print(f"Sarah's favorite langauge is: '{language}'")
+print()
+
+print("*****test built-in dictionary get() method*****")
+fav_lang = favorite_languages.get('jen', 'sorry, no team member with that name').title()
+print(f"Jen's favorite language is: '{fav_lang}'")
+fav_lang = favorite_languages.get('Lg', 'sorry, no team member with that name').title()
+print(f"Lg's favorite language is: '{fav_lang}'")
+print()
