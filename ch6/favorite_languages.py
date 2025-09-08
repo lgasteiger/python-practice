9866f6b80c05b8b56e812cc346ba7a4643170a1e
lgasteiger <lgasteiger@gmail.com>
2025-09-07 18:57:12 -0700

    refactored the friends favorite programming languages python 3 app to support dictionary keys searching

--- ch6/favorite_languages.py
+++ ch6/favorite_languages.py
@@ -12,6 +12,35 @@ def get_language(dic_name, dic_key):
     return dic_name[dic_key].title()
 # end get_language()
 
+def get_lang_keys(dic_name):
+    """
+    this function will print only the keys of the programming language's keys
+    """
+    for name in dic_name.keys():
+        print(name.title())
+    # end for
+# end get_lang_keys()
+        
+def check_friend_keys(dic_name):
+    """
+    this function will determine if certain friends are "keys" in the dic_name
+    Python 3 dictionary data structure
+    """
+    curr_friends = ['phil', 'sarah']
+
+    for friend_name in dic_name.keys():
+        print(f"hi {friend_name.title()}")
+
+        if friend_name in curr_friends:
+            fav_lang = dic_name[friend_name].title()
+            print(
+                f"\t{friend_name.title()}, i see you love the {fav_lang} " +
+                f"programming language!"
+            ) # end print()
+        # end if
+    # end for
+# end check_friend_keys(dic_name)
+
 ############################
 # main program starts here #
 ############################
@@ -33,3 +62,10 @@ print(f"Jen's favorite language is: '{fav_lang}'")
 fav_lang = favorite_languages.get('Lg', 'sorry, no team member with that name').title()
 print(f"Lg's favorite language is: '{fav_lang}'")
 print()
+
+print("*****test printing dictionary keys only*****")
+get_lang_keys(favorite_languages)
+print()
+
+print("*****test working with dictionary keys comparisons*****")
+check_friend_keys(favorite_languages)
